4d39de335aa180e28c3085b8608a7d82f6c735c4
Versadinary <170194434+versadinary@users.noreply.github.com>
2024-08-07 19:51:22 +0300

    add start button functionality

---
+++ rps-ui/script.js
@@ -0,0 +1,6 @@
+const startBtn = document.querySelector(".start");
+
+startBtn.addEventListener('click', () => {
+    const startcont = document.querySelector(".start-container");
+    startcont.style.display = 'none';
+});
