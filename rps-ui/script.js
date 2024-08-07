4d82d5de918a5a8407ffa1439ad5ce7423db68a0
Versadinary <170194434+versadinary@users.noreply.github.com>
2024-08-07 21:21:56 +0300

    add button start game button

--- rps-ui/script.js
+++ rps-ui/script.js
@@ -2,5 +2,7 @@ const startBtn = document.querySelector(".start");
 
 startBtn.addEventListener('click', () => {
     const startcont = document.querySelector(".start-container");
+    const gamecont = document.querySelector(".game-container");
     startcont.style.display = 'none';
+    gamecont.style.display = 'flex';
 });
